6813affc1e797820cc8198f5c8a80bfa79b0707e
Pradeep4157 <krp18539@gmail.com>
2026-03-31 13:36:58 +0530

    done par with agg...

--- Practical_OOP/OBJECT_RELATION/AGGREGATION.cpp
+++ Practical_OOP/OBJECT_RELATION/AGGREGATION.cpp
@@ -17,33 +17,42 @@ using namespace std;
 
 */
 
-// Object 1
-class Person
-{
-public:
-    Person(string person_name_) : person_name(person_name_) {}
-    void Breathe();
-    void Walk();
-    ~Person()
-    {
-        cout << "~Human" << endl;
-    }
-
-private:
-    string person_name;
-};
 class Home
 {
 public:
     Home(string address_) : address(address_) {}
+    ~Home()
+    {
+        cout << "~Home of address: " << address << endl;
+    }
 
 private:
     string address;
 };
+// Object 1
+class Person
+{
+public:
+    Person(string person_name_, Home *home_) : person_name(person_name_), home(home_) {}
+    void Breathe();
+    void Walk();
+    ~Person()
+    {
+        cout << "~Person " << person_name << endl;
+    }
+
+private:
+    Home *home;
+    string person_name;
+};
+
 signed main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    Home home("Adress_1");
+    Person *person1 = new Person("A", &home);
+    Person *person2 = new Person("B", &home);
 
     return 0;
 }
