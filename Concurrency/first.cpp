1055a077b3f37363881f8424ba21ad20e289b1ae
Pradeep4157 <krp18539@gmail.com>
2026-07-09 14:24:08 +0530

    par done with lambda, emplace insertions..

---
+++ Concurrency/first.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+
+int main()
+{
+    std::cout << "Main thread id : " << std::this_thread::get_id() << std::endl;
+    unsigned hc = std::thread::hardware_concurrency();
+    std::cout << "Hardware Concurrency is : " << hc << "\n";
+    const int N = 4;
+    std::vector<std::thread> threads;
+    threads.reserve(N);
+    for (int i = 0; i < N; i++)
+    {
+        threads.emplace_back([i]()
+                             {
+                                 std::this_thread::sleep_for(std::chrono::milliseconds(10 * (i + 1)));
+                                 std::cout << "Worker #" << i << " thread id: " << std::this_thread::get_id() << "\n";
+                             });
+    }
+}
