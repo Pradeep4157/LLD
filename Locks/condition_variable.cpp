4ad72588c958c9fc512ec108de27bdc6f9856806
Pradeep4157 <krp18539@gmail.com>
2026-07-31 13:32:30 +0530

    par done with cv and unq lock..

--- Locks/condition_variable.cpp
+++ Locks/condition_variable.cpp
@@ -26,8 +26,33 @@ void producer()
     }
     cv.notify_all();
 }
+void consumer(int id)
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> lk(mtx);
+        cv.wait(lk, []
+                { return !q.empty() || done; });
+        if (!q.empty())
+        {
+            int item = q.front();
+            q.pop();
+            lk.unlock();
+            std::cout << "Consumer " << id << " got: " << item << "\n";
+        }
+        else if (done)
+        {
+            break;
+        }
+    }
+}
 int main()
 {
+    std::thread p(producer);
+    std::thread c1(consumer, 1), c2(consumer, 2);
+    p.join();
+    c1.join();
+    c2.join();
 
     return 0;
 }
