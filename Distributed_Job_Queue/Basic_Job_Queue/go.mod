9bd2e64c2b3d7c75cda1046205d506c3232329d6
Pradeep4157 <krp18539@gmail.com>
2026-09-08 13:29:50 +0530

    par done with dist j q..

---
+++ Distributed_Job_Queue/Basic_Job_Queue/go.mod
@@ -0,0 +1,3 @@
+module Basic_Job_Queue
+
+go 1.26.6
---
+++ Distributed_Job_Queue/Basic_Job_Queue/server/main.go
@@ -0,0 +1,25 @@
+// every go file belongs to package, main means that it is an executable program.. 
+package main
+// fmt is for printing things and http is for creating an http server..
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// when any request comes on /job handleJob function is going to be executed.. 
+	http.HandleFunc("/job", handleJob)
+	// just printing on console..
+	fmt.Println("Queue Server running on port 8080")
+	// starts server on port 8080
+	http.ListenAndServe(":8080", nil)
+}
+
+// function is executed whenever any request comes on /job endpoint..
+// r is info regarding incoming request, w allows us to send some response back to client.. 
+func handleJob(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received a Job!")
+	// here we are sending the response back to client, []byte is just that we convert the response in 
+	// array of byte and return.. 
+	w.Write([]byte("Job received"))
+}
